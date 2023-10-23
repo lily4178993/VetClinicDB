764b01dcf72745fe8e6442a06acb3ba8c66359be
lily4178993 <nellytelli@gmail.com>
2023-10-23 18:32:16 +0000

    feat(db): create 'animals' table
    This commit adds the SQL script for creating the 'animals' table in the database. It includes columns for 'id', 'name', 'date_of_birth', 'escape_attempts', 'neutered', and 'weight_kg'. Comments have been added to provide documentation for the table and its columns.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals
+(
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg NUMERIC(5, 2) NOT NULL
 );
+
+-- Add a comment to the "animals" table
+COMMENT ON TABLE animals IS 'Table containing information about animals in the vet clinic';
+
+-- Add comments to the columns of the "animals" table
+COMMENT ON COLUMN animals.id IS 'Unique identifier for each animal';
+COMMENT ON COLUMN animals.name IS 'Name of the animal';
+COMMENT ON COLUMN animals.date_of_birth IS 'Date of birth of the animal';
+COMMENT ON COLUMN animals.escape_attempts IS 'Number of escape attempts made by the animal';
+COMMENT ON COLUMN animals.neutered IS 'Indicates if the animal is neutered (true/false)';
+COMMENT ON COLUMN animals.weight_kg IS 'Weight of the animal in kilograms';
